c484b142bf1de8ec9f5d722a762e5c60fcf3ec18
Anton Bilyi <anthonybelyj@protonmail.com>
2025-05-17 20:33:08 +0300

    Done HW 11

---
+++ src/main.js
@@ -0,0 +1,50 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+import { createGallery, clearGallery, showLoader, hideLoader } from "./js/render-function";
+import { getImagesByQuery } from "./js/pixabay-api";
+
+const formEl = document.querySelector('.form');
+const galleryEl = document.querySelector('.gallery');
+const loaderEl = document.querySelector('.loader');
+
+
+formEl.addEventListener('submit', e => {
+    e.preventDefault();
+
+    clearGallery();
+
+    const query = e.target.elements['search-text'].value.trim();
+
+    showLoader();
+
+    getImagesByQuery(query)
+        .then(data => {
+            const images = data.hits;
+
+            if (images.length === 0) {
+                iziToast.warning({
+                    title: 'Sorry',
+                    message: 'There are no images matching your search query. Please try again!',
+                    position: 'topRight',
+                    timeout: 3000,
+                });
+                return;
+            }
+
+            createGallery(images);
+            formEl.reset();
+        })
+        .catch(error => {
+            console.error('Помилка при отриманні зображень:', error.message);
+            iziToast.error({
+                title: 'Error',
+                message: 'Failed to fetch images. Please try again later.',
+                position: 'topRight',
+                timeout: 3000,
+            });
+        })
+        .finally(() => {
+            hideLoader();
+        });
+});
